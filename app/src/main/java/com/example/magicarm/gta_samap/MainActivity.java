d21c6941d61204627a929f90907b338006b776be
magicarm22 <magicarm22@rambler.ru>
2019-04-26 16:07:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/magicarm/gta_samap/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.magicarm.gta_samap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
